857e0ed87492281ace980a37d29c2880001b9792
surendra <chavidisurendra@gmail.com>
2025-05-13 11:12:49 +0530

    msg

--- specialvarables.sh
+++ specialvarables.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-
 echo "allvariables : $@"
 echo " number of variables passed : $#"
 echo "script name : $0"
@@ -9,3 +7,5 @@ echo "Home Directory of current user: $HOME"
 echo "which user is running: $USER"
 echo "hostname: $HOSTNAME"
 echo "process id of current shell script: $$"
+echo "process id of last background command: $!"
+sleep 60 &
