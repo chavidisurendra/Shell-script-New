40c2f6a4de60283794e40cc001b2977dca1a0b4b
surendra <chavidisurendra@gmail.com>
2025-05-13 11:00:28 +0530

    msg

--- variables.sh
+++ variables.sh
@@ -5,8 +5,13 @@ echo "$person1:: Hello $person1"
 
 echo "please enter username"
 
-read USERNAME
+read -s USERNAME
 
 echo "please enter password"
 
-read PASSWORD
+read -s PASSWORD
+
+#dAtatypes
+
+movies=("rrr" "dj")
+echo "first movie is: ${movies[0]}"
